00fb3267de5f4bca64cd43831203363e34c509ba
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-01-24 02:42:33 +0900

    update features | memo app

--- portfolio_app/memo/urls.py
+++ portfolio_app/memo/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'memo'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('project/<str:pk>/', views.project, name='project'),
+    path('update-memo/<str:pk>/', views.update_memo, name='update-memo'),
+    path('delete-memo/<str:pk>/', views.delete_memo, name='delete-memo'),
 ]
